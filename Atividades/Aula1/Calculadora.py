a4c228649eb60b543c0b5a5ab773685071357692
Corongozo <phenriquefraga01@gmail.com>
2025-10-24 15:53:05 -0300

    Primeiro commit (Aula1)

---
+++ Atividades/Aula1/Calculadora.py
@@ -0,0 +1,12 @@
+num1=2
+num2=4
+soma=num1+num2
+sub=num1-num2
+div=num1/num2
+mult=num1*num2
+mod=num1%num2
+print("Soma: ", soma)
+print("Subtracao: ", sub)
+print("Divisao: ", div)
+print("Multiplicacao: ", mult)
+print("Modulo: ", mod)
---
+++ Atividades/Aula1/MediaBoletim.py
@@ -0,0 +1,4 @@
+nota1=5.5
+nota2=9
+media=(nota1 + nota2)/2
+print(media)
