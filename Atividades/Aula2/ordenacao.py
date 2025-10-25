735d72ba400c286470c2a0da574a314ec39f4aed
Corongozo <107891702+Corongozo@users.noreply.github.com>
2025-10-24 21:57:20 -0300

    Create ordenacao.py

---
+++ Atividades/Aula2/ordenacao.py
@@ -0,0 +1,9 @@
+# Sistema de ordenação de numeros
+num1=float(input('Primeiro valor: '))
+num2=float(input('Segundo valor: '))
+num3=float(input('Terceiro valor: '))
+
+if num1<num2 and num1<num3
+    
+elif 
+# A ser feito
