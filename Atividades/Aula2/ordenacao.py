40249cb3ecd64ed5faddd9f29a3f878436ce4f95
Corongozo <phenriquefraga01@gmail.com>
2025-10-27 16:14:40 -0300

    Update ordenacao.py

--- Atividades/Aula2/ordenacao.py
+++ Atividades/Aula2/ordenacao.py
@@ -3,7 +3,43 @@ num1=float(input('Primeiro valor: '))
 num2=float(input('Segundo valor: '))
 num3=float(input('Terceiro valor: '))
 
-if num1<num2 and num1<num3
-    
-elif 
-# A ser feito
+if num1<num2 and num1<num3:
+    slot1=num1
+elif num1>num2 and num1<num3:
+    slot2=num1
+    slot1=num2
+    slot3=num3
+elif num1<num2 and num1>num3:
+    slot2=num1
+    slot1=num3
+    slot3=num2
+elif num1>num2 and num1>num3:
+    slot3=num1
+
+if num2<num1 and num2<num3:
+    slot1=num2
+elif num2>num1 and num2<num3:
+    slot2=num2
+    slot1=num1
+    slot3=num3
+elif num2<num1 and num2>num3:
+    slot2=num2
+    slot1=num3
+    slot3=num1
+elif num2>num1 and num2>num3:
+    slot3=num2
+
+if num3<num1 and num3<num2:
+    slot1=num3
+elif num3>num1 and num3<num2:
+    slot2=num3
+    slot1=num1
+    slot3=num2
+elif num3<num1 and num3>num2:
+    slot2=num3
+    slot1=num2
+    slot3=num1
+elif num3>num1 and num3>num2:
+    slot3=num3
+
+print('Numeros em ordem:',slot1, slot2, slot3)
