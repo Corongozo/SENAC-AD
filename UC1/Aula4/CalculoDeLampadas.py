9cc928a4a186bc5f7655f37b17667511fead7592
Corongozo <107891702+Corongozo@users.noreply.github.com>
2025-10-29 21:47:25 -0300

    Calculo incompleto.py

---
+++ UC1/Aula4/CalculoDeLampadas.py
@@ -0,0 +1,9 @@
+# 1. Cálculo de Lâmpadas: 
+# Escreva um programa para calcular e imprimir o número de lâmpadas necessárias para 
+# iluminar um determinado cômodo de uma residência. Dados de entrada: a potência da 
+# lâmpada utilizada (em watts), as dimensões (largura e comprimento, em metros) do 
+# cômodo. Considere que a potência necessária é de 3 watts por metro quadrado e a cada 
+# 3m² existe um bocal para uma lâmpada.
+potencia=float(input('Informe a potencia da lampada: '))
+largura=float(input('Informe a largura do comodo: '))
+comprimento=float(input('Informe a comprimento do comodo: '))
