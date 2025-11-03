42b3d7e0a9fe9413d5e98f28ea93e4685ce150ec
Corongozo <107891702+Corongozo@users.noreply.github.com>
2025-11-03 18:26:39 -0300

    bleh

---
+++ UC1/Aula5/Desafio1.py
@@ -0,0 +1,3 @@
+# 1. Cálculo de Média Escolar para Vários Alunos 
+# Use o laço for para repetir a lógica de cálculo de média e status 
+# (Aprovado/Reprovado/Recuperação) que você fez na Aula 4, agora para 10 estudantes.
