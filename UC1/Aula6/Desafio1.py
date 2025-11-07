25dee34b4338040b347ca55771edd696cefeb42a
Corongozo <107891702+Corongozo@users.noreply.github.com>
2025-11-07 20:09:06 -0300

    eh?

--- UC1/Aula6/Desafio1.py
+++ UC1/Aula6/Desafio1.py
@@ -2,33 +2,37 @@
 # Use um for loop para iterar 5 vezes. Dentro do loop, realize a leitura das notas e a decisão 
 # (if/elif/else) da média. Crie uma lista vazia (resultados = []). A cada repetição, adicione uma 
 # string (ex: "Aluno 1 - Aprovado") a esta lista usando .append().
-resultado = []
-for i in range(2):
-    try:
-        print('-'*90,)
-        print(f'Aluno {i+1}:')
-        nota1 = float(input("Digite a nota da primeira avaliação: "))
-        nota2 = float(input("Digite a nota da segunda avaliação: "))
-        optativa = float(input("Digite a nota da avaliação optativa (ou -1 se não fez): "))
+def optativa(num_de_estudantes):
+    resultado = []
+    for i in range(num_de_estudantes):
+        try:
+            print('-'*90,)
+            print(f'Aluno {i+1}:')
+            nota1 = float(input("Digite a nota da primeira avaliação: "))
+            nota2 = float(input("Digite a nota da segunda avaliação: "))
+            optativa = float(input("Digite a nota da avaliação optativa (ou -1 se não fez): "))
 
-        # Substituição da menor nota, se a optativa foi feita
-        if optativa != -1:
-            if nota1 < nota2:
-                nota1 = optativa
+            # Substituição da menor nota, se a optativa foi feita
+            if optativa != -1:
+                if nota1 < nota2:
+                    nota1 = optativa
+                else:
+                    nota2 = optativa
+
+            # Cálculo da média
+            media = (nota1 + nota2) / 2
+
+            if media >= 6.0:
+                resultado.append(f"Aluno {i+1} Situação: Aprovado")
+            elif media < 3.0:
+                resultado.append(f"Aluno {i+1} Situação: Reprovado")
             else:
-                nota2 = optativa
+                resultado.append(f"Aluno {i+1} Situação: Recuperação")
+        except ValueError:
+            print("Valor invalido.")
+    for i in resultado:
+        print('-'*90)
+        print(i)
 
-        # Cálculo da média
-        media = (nota1 + nota2) / 2
 
-        if media >= 6.0:
-            resultado.append(f"Aluno {i+1} Situação: Aprovado")
-        elif media < 3.0:
-            resultado.append(f"Aluno {i+1} Situação: Reprovado")
-        else:
-            resultado.append(f"Aluno {i+1} Situação: Recuperação")
-    except ValueError:
-        print("Valor invalido.")
-for i in resultado:
-    print('-'*90)
-    print(i)
+optativa(2)
--- UC1/Aula6/Desafio2.py
+++ UC1/Aula6/Desafio2.py
@@ -3,26 +3,31 @@
 # candidato é menor de 18 anos (rejeição). Crie uma lista principal: candidatos_validos = []. 
 # Se o candidato for válido, crie um Dicionário (ex: candidato = {'nome': '...', 'email': '...'}). 
 # Adicione este Dicionário à lista: candidatos_validos.append(candidato). 
-ano_atual = 2025
-candidatos_validos = []
+def cadastro(candidatos):
+    ano_atual = 2025
+    candidatos_validos = []
 
-for i in range(5):
-    try:
-        print('-'*90, "\nCandidato", i+1)
-        ano_nascimento = int(input("Digite o ano de nascimento: "))
-        idade = ano_atual - ano_nascimento
+    for i in range(candidatos):
+        try:
+            print('-'*90, "\nCandidato", i+1)
+            ano_nascimento = int(input("Digite o ano de nascimento: "))
+            idade = ano_atual - ano_nascimento
 
-        if idade < 18:
-            print("Candidato menor de 18 anos. Não pode participar.")
-            continue  # pula para o próximo candidato
+            if idade < 18:
+                print("Candidato menor de 18 anos. Não pode participar.")
+                estado = False
+                candidato = {'nome': i, 'estado': estado}
+                continue
 
-        # Coleta dos demais dados
-        telefone = input("Digite o telefone: ")
-        email = input("Digite o e-mail: ")
-        candidato = {'nome': i, 'email': email, 'telefone': telefone}
-        candidatos_validos.append(candidato)
-        print("Dados coletados com sucesso!")
-    except ValueError:
-        print("Valor inválido.")
-for i in candidatos_validos:
-    print(i)
+            # Coleta dos demais dados
+            telefone = input("Digite o telefone: ")
+            email = input("Digite o e-mail: ")
+            estado = True
+            candidato = {'nome': i, 'email': email, 'telefone': telefone, 'estado': estado}
+            candidatos_validos.append(candidato)
+            print("Dados coletados com sucesso!")
+        except ValueError:
+            print("Valor inválido.")
+    for i in candidatos_validos:
+        print(i)
+cadastro(2)
